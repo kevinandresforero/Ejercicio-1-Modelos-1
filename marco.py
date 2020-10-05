39ad12381fb3424012e1e87cb4f6dd5ede5d781b
kevinandresforero <kevinandresforero@gmail.com>
2020-10-05 17:21:11 -0500

    First point

---
+++ marco.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 16:41:06 2020
+
+@author: KEV
+"""
+
+import tkinter as tk
+
+class marco:
+    def __init__():
+        marco = tk.Tk()
+        marco.title("Polinomio Entero")
+        marco.geometry("500x500")
+        marco.resizable(0,1)
+        marco.configure(background="#000000")
+        return marco
+    
+"""m = marco.__init__()
+m.mainloop()
+        """
